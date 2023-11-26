889069364404555435f422beca3bc4f70e2a47b2
Asadullah Khalid <Asad-Ullah.Khalid@telekom.de>
2023-11-25 16:07:52 -0800

    remove old files and add backend for smart contract interaction

--- week3/TokenVotes/contracts/MyNFT.sol
+++
@@ -1,8 +0,0 @@
-// SPDX-License-Identifier: MIT
-pragma solidity >=0.7.0 <0.9.0;
-
-import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
-
-contract MyNFT is ERC721 {
-    constructor() ERC721("MyNFT", "MNF") {}
-}
